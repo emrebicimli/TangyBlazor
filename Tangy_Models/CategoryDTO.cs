aaa05943e89b99366afba4dad6e89f1c177d9d5d
emrebicimli <emrebicimli@gmail.com>
2022-08-25 12:44:36 +0100

    automapper error

--- Tangy_Models/CategoryDTO.cs
+++ Tangy_Models/CategoryDTO.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Tangy_Models
 {
     public class CategoryDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter name..")]
         public string Name { get; set; }
     }
 }
